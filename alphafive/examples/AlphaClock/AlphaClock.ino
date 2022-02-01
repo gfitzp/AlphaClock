4442a551b47b3b7909702ad1d012d0dd7ef8a536
Glenn Fitzpatrick <glenn@glennfitzpatrick.com>
2022-01-31 20:07:29 -0500

    Added documentation and citations

--- alphafive/examples/AlphaClock/AlphaClock.ino
+++ alphafive/examples/AlphaClock/AlphaClock.ino
@@ -1086,6 +1086,38 @@ void setup() {
 
   EEReadSettings(); // Read settings stored in EEPROM
 
+  /*
+    Figured out a lot of the GPS code from https://github.com/adafruit/Adafruit_GPS/
+    examples.
+
+    Software License Agreement (BSD License)
+
+    Copyright (c) 2012, Adafruit Industries
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+    1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holders nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  */
+
   if (GPSMode) {
     GPS.begin(9600);
     GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); // needs to be PMTK_SET_NMEA_OUTPUT_RMCGGA otherwise we don't get the number of satellites we can currently see
@@ -1197,6 +1229,8 @@ void loop() {
 
   milliTemp = millis();
   checkButtons();
+
+  // More code building on the https://github.com/adafruit/Adafruit_GPS examples...
 
   if (GPSMode) {
 
